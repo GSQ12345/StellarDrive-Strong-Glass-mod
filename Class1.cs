b17e4ba1cbaf94dd87d5e2f9b3fbd6c2bb8c6a12
GSQ123 <162472720+GSQ12345@users.noreply.github.com>
2026-09-20 18:08:44 +0200

    class1

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using MelonLoader;
+using Planets.Terrain.Meshes;
+using Planets.Terrain.Meshes.Colliders;
+using Ships.Interface.Values;
+using Ships.Parts.Common.Model;
+using Ships.Physics.Collision;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(Unbreakable_Glass.Class1), "Strong Glass", "1.0.0", "GSQ123")]
+[assembly: MelonGame(null, null)]
+
+namespace Unbreakable_Glass
+{
+    public class Class1 : MelonMod
+    {
+
+        
+        public override void OnInitializeMelon()
+        {
+            HullConstants.GlassHardness = 100f;
+        }
+    }
+}
